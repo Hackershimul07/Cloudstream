1d156bd70b61258c615cf03220755e34f033fb7c
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-08 02:27:09 +0600

    Update build.gradle.kts

--- CineFreak/build.gradle.kts
+++ CineFreak/build.gradle.kts
@@ -1,25 +1,26 @@
 // use an integer for version numbers
 version = 1
 
+
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    language = "bn"
     authors = listOf("Shimul_Ahmed")
-
+    description ="Bangla/Hindi Movies/Series"
     /**
-     * Status int as one of the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta-only
-     **/
-    status = 1 // Will be 3 if unspecified
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
 
     tvTypes = listOf(
         "Movie",
         "TvSeries",
         "Anime"
     )
-    iconUrl = "https://cinefreak.nl/wp-content/uploads/2024/08/cropped-cgk-270x270.png"
+    language = "bn"
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/cinefreak.png"
+
+    isCrossPlatform = true
 }

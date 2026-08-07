301d120d6969d135209ddc20745802d488f1d920
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-08 00:20:02 +0600

    Update TenHitMoviesProvider.kt

--- 9kMovies/src/main/kotlin/com/redowan/TenHitMoviesProvider.kt
+++ 9kMovies/src/main/kotlin/com/redowan/TenHitMoviesProvider.kt
@@ -11,7 +11,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
 
 class TenHitMoviesProvider : MainAPI() {
-    override var mainUrl = "https://10hitmovies.study"
+    override var mainUrl = "https://10hitmovies.study/"
     override var name = "10HitMovies"
     override var lang = "bn"
     override val hasMainPage = true
@@ -36,8 +36,13 @@ class TenHitMoviesProvider : MainAPI() {
     page: Int,
     request: MainPageRequest
 ): HomePageResponse {
-    val path = if (request.data.isEmpty()) "/" else request.data
-val doc = app.get("$mainUrl${path}page/$page").document
+    val url = if (request.data.isEmpty()) {
+        "${mainUrl}page/$page"
+    } else {
+        "$mainUrl${request.data.removePrefix("/") }page/$page"
+    }
+
+    val doc = app.get(url).document
     val home = doc.select(".thumb.col-md-2.col-sm-4.col-xs-6").mapNotNull { toResult(it) }
     return newHomePageResponse(request.name, home, hasNext = true)
 }

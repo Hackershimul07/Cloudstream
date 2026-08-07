d7ac03bc818f8ea3a09b26c6be3dfe41acbbeec1
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-08 00:58:38 +0600

    Update Mp4MoviezProvider.kt

--- Mp4Moviez/src/main/kotlin/com/Shimul/Mp4MoviezProvider.kt
+++ Mp4Moviez/src/main/kotlin/com/Shimul/Mp4MoviezProvider.kt
@@ -29,8 +29,8 @@ class Mp4MoviezProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries, TvType.NSFW)
     override val mainPage = mainPageOf(
         "" to "Latest Updates",
-        "/4/south-indian-hindi-dubbed-movies.html" to "Hindi Dubbed",
-        "/276/bangladeshi-movies.html" to "Bangladeshi Movies",
+        "/360/latest-south-indian-hindi-dubbed-movies-(2026).html" to "Hindi Dubbed",
+        "/362/latest-bangladeshi-movies-(2026).html" to "Bangladeshi Movies",
         "/189/netflix-series-hindi-dubbed.html" to "New Hindi Dubbed Series",
     )
 

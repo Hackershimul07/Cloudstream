52b381d4d2f1e5e23865ebbb8f7294625ed32238
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-08 11:55:45 +0600

    Create build.gradle.kts

---
+++ PornHub/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("Shimul_Ahmed")
+    language    = "bn"
+    description = "Welcome to Pornhub.com, home of the best hardcore free porn videos with the hottest adult stars. Get full length scenes from your favorite porn studios 24/7!"
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.pornhub.com/&size=128"
+}

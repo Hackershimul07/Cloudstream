84a2a3db06b7dbc755c9e3c11353be7e0d525510
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-08 12:22:58 +0600

    Initialize build.gradle.kts for FamilyPorn project
    Added configuration for FamilyPorn project including version, authors, language, description, status, tvTypes, and iconUrl.

---
+++ FamilyPorn/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("Shimul_Ahmed")
+    language    = "bn"
+    description = "FamilyPornHD It's a natural mix of the neighborhood stepmoms, older stepdads, barely legal stepsisters, and even the college going stepbrother falling into some serious family fun."
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=familypornhd.com&sz=%size%"
+}

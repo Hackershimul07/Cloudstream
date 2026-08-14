ab78176c20a77e02fbc89bc9fbd54c843011a9c4
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-14 20:30:30 +0600

    Update build.gradle.kts

--- MovieLinkBDProvider/build.gradle.kts
+++ MovieLinkBDProvider/build.gradle.kts
@@ -1,4 +1,3 @@
-// use an integer for version numbers
 version = 1
 
 android {
@@ -7,20 +6,16 @@ android {
     }
 }
 
+dependencies {
+    compileOnly("org.jspecify:jspecify:1.0.0")
+}
+
 cloudstream {
     description = "MovieLinkBD - Largest Movie Download Site in Bangladesh"
     authors = listOf("Shimul_Ahmed")
 
-    /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
     status = 1
 
-    // List of video source types.
     tvTypes = listOf(
         "Movie",
         "TvSeries",

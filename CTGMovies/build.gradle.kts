eaeb67bed33656d5e475701b028a21a1a554255d
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-15 23:53:52 +0600

    Update build.gradle.kts

--- CTGMovies/build.gradle.kts
+++ CTGMovies/build.gradle.kts
@@ -8,6 +8,7 @@ android {
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("org.jspecify:jspecify:1.0.1")
 }
 
 cloudstream {

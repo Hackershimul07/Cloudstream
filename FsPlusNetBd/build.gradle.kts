ad251a5245b51c97ae479b505d9599a590f53c92
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-15 13:08:01 +0600

    Create build.gradle.kts

---
+++ FsPlusNetBd/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    language = "bn"
+    // All of these properties are optional, you can safely remove them
+
+    description = "BDIX file server (Movies & Shows)"
+    authors = listOf("Shimul_Ahmed")
+
+    /**
+     * Status int as follows:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     */
+    status = 1
+
+    tvTypes = listOf("Movie", "TvSeries")
+
+    iconUrl = "https://fs.plus.net.bd/favicon.ico"
+}

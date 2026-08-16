bd7a483001f7ece6ac6f6a765f449ac5c25cc4d0
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-17 03:41:58 +0600

    Update FMFTPProvider.kt

--- FMFTP/src/main/kotlin/com/niloy/FMFTPProvider.kt
+++ FMFTP/src/main/kotlin/com/niloy/FMFTPProvider.kt
@@ -173,7 +173,9 @@ class FMFTPProvider : MainAPI() {
         //"$mainUrl/api/movies?limit=20&fields=id,title,genre,year,views,download,online_rating,release_date,poster_path,backdrop_path&library=2&page=1&sort=release_date" to "Hollywood Movies",
         //"$mainUrl/api/tv-shows?limit=20&library=9&page=1&sort=release_date" to "English TV Shows",
         "$mainUrl/api/tv-shows?limit=20&library=10&page=1&sort=release_date" to "Hindi TV Shows",
-        //"$mainUrl/api/tv-shows?limit=20&library=11&page=1&sort=release_date" to "Korean TV Shows"
+        //"$mainUrl/api/tv-shows?limit=20&library=11&page=1&sort=release_date" to "Korean TV Shows",
+        "$mainUrl/api/tv-shows?limit=20&library=12&page=1&sort=release_date" to "Bangla TV Shows",
+        "$mainUrl/api/movies?limit=20&fields=id,title,genre,year,views,download,online_rating,release_date,poster_path,backdrop_path&library=3&page=1&sort=release_date" to "Animation"
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {

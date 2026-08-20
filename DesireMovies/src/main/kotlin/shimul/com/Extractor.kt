e67db9cf459a814683ffc715fc9d0b42c99f07b9
SHIMUL AHMED <95375960+Hackershimul07@users.noreply.github.com>
2026-08-20 14:52:19 +0600

    Update Extractor.kt

--- DesireMovies/src/main/kotlin/shimul/com/Extractor.kt
+++ DesireMovies/src/main/kotlin/shimul/com/Extractor.kt
@@ -3,6 +3,7 @@ package shimul.com
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
 class MyCustomExtractor : ExtractorApi() {
     override val name = "DesireMovies Extractor"
@@ -12,13 +13,14 @@ class MyCustomExtractor : ExtractorApi() {
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
         // এখানে আপনার লিঙ্ক ডিকোড করার লজিক থাকবে
         return listOf(
-            ExtractorLink(
-                this.name,
-                this.name,
-                url, // আসল ভিডিও লিঙ্ক
-                referer ?: "",
-                Qualities.P1080.value
-            )
+            newExtractorLink(
+                source = this.name,
+                name = this.name,
+                url = url // আসল ভিডিও লিঙ্ক
+            ) {
+                this.referer = referer ?: ""
+                this.quality = Qualities.P1080.value
+            }
         )
     }
 }
